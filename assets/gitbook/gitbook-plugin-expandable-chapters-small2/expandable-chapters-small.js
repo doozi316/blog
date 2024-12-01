14b4566c288fd0beacf380ab72d15063c2b7fb6f
김현진 <doozi316@gmail.com>
2024-12-02 02:15:40 +0900

    트리 이벤트 변경

--- assets/gitbook/gitbook-plugin-expandable-chapters-small2/expandable-chapters-small.js
+++ assets/gitbook/gitbook-plugin-expandable-chapters-small2/expandable-chapters-small.js
@@ -1,13 +1,10 @@
 require(['gitbook', 'jQuery'], function (gitbook, $) {
     var PLUGIN = 'expandable-chapter-small2',
         TOGGLE_CLASSNAME = 'expanded',
-        CHAPTER = '.chapter',
-        // ARTICLES = '.articles',
-        ARTICLES = '.chapter ul',
-        FOLDABLE = '.chapter, .chapter li',
-        ARTICLE_CHILDREN = 'ul',
-        TRIGGER_TEMPLATE = '<i class="exc-trigger fa"></i>',
-        LS_NAMESPACE = 'expChapters';
+        CATEGORY = '.category',
+        CATEGORY_TITLE = '.category-title',
+        POST_TITLE = '.post-title',
+        POST = '.post';
 
     var init = function () {
         // adding the trigger element to each ARTICLES parent and binding the event
@@ -17,86 +14,44 @@ require(['gitbook', 'jQuery'], function (gitbook, $) {
             articlesExpand = config[PLUGIN].articlesExpand || false;
         }
         if (articlesExpand) {
-            $(ARTICLES)
-                .parent(CHAPTER)
-                .find(ARTICLE_CHILDREN)
-                .prev()
+            $(CATEGORY_TITLE)
                 .css('cursor', 'pointer')
                 .on('click', function (e) {
                     e.preventDefault();
                     e.stopPropagation();
-                    toggle($(e.target).closest(FOLDABLE));
-                })
-                .append(TRIGGER_TEMPLATE);
+                    toggle($(e.target).closest(CATEGORY));
+                });
+            $(POST_TITLE)
+                .css('cursor', 'pointer')
+                .on('click', function (e) {
+                    toggle($(e.target).closest(POST));
+                });
+        }
+    };
+
+    var toggle = function ($category) {
+        if ($category.hasClass('expanded')) {
+            collapse($category);
         } else {
-            $(ARTICLES)
-                .parent(CHAPTER)
-                .find(ARTICLE_CHILDREN)
-                .prev()
-                .append(
-                    $(TRIGGER_TEMPLATE)
-                        .on('click', function (e) {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            toggle($(e.target).closest(FOLDABLE));
-                        })
-                );
+            expand($category);
         }
-        expand(lsItem());
+    };
 
-        // expand current selected chapter with it's parents
-        var activeChapter = $(CHAPTER + '.active');
-        expand(activeChapter);
-
-        // expand current selected chapter's children
-        // expand(activeChapter.parents(CHAPTER));
-        activeChapter.find(ARTICLE_CHILDREN).closest(FOLDABLE).each(function () {
-            expand($(this));
-        });
-    }
-
-    var toggle = function ($chapter) {
-        if ($chapter.hasClass('expanded')) {
-            collapse($chapter);
-        } else {
-            expand($chapter);
+    var collapse = function ($category) {
+        if ($category.length && $category.hasClass(TOGGLE_CLASSNAME)) {
+            $category.removeClass(TOGGLE_CLASSNAME);
+            $category.find('ul').css('display', 'none');
         }
-    }
+    };
 
-    var collapse = function ($chapter) {
-        if ($chapter.length && $chapter.hasClass(TOGGLE_CLASSNAME)) {
-            $chapter.removeClass(TOGGLE_CLASSNAME);
-            lsItem($chapter);
+    var expand = function ($category) {
+        if ($category.length && !$category.hasClass(TOGGLE_CLASSNAME)) {
+            $category.addClass(TOGGLE_CLASSNAME);
+            $category.find('ul').css('display', 'block');
         }
-    }
-
-    var expand = function ($chapter) {
-        if ($chapter.length && !$chapter.hasClass(TOGGLE_CLASSNAME)) {
-            $chapter.addClass(TOGGLE_CLASSNAME);
-            lsItem($chapter);
-        }
-    }
-
-    var lsItem = function () {
-        var map = JSON.parse(localStorage.getItem(LS_NAMESPACE)) || {}
-        if (arguments.length) {
-            var $chapters = arguments[0];
-            $chapters.each(function (index, element) {
-                var level = $(this).data('level');
-                var value = $(this).hasClass(TOGGLE_CLASSNAME);
-                map[level] = value;
-            })
-            localStorage.setItem(LS_NAMESPACE, JSON.stringify(map));
-        } else {
-            return $(CHAPTER).map(function (index, element) {
-                if (map[$(this).data('level')]) {
-                    return this;
-                }
-            })
-        }
-    }
+    };
 
     gitbook.events.bind('page.change', function () {
-        init()
+        init();
     });
 });
